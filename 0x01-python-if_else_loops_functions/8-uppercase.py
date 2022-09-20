402288656fb922f585f2708e426c220ad6d6aa9c
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-20 20:50:04 +0000

    added a few files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,5 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = ord(i) - 32
-            print("{}".format(chr(i)), end="")
-        else:
-            print("{}".format(i))
-
-str = "heLLo"
-print(uppercase(str))
-        
+        print("{}".format(chr(i)), end="")
+    print("")
