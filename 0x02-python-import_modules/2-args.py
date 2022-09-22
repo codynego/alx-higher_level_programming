cf7fe8032ade6c297febb9c14deae18a8bfe5ef9
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-22 20:31:24 +0000

    quick fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-no_of_args = len(sys.argv) - 1
-if no_of_args == 1:
-    print("{} argument:".format(no_of_args))
-elif no_of_args == 0:
-    print("{} argument.".format(no_of_args), end="")
-elif no_of_args > 1:
-    print("{} arguments:".format(no_of_args))
+if __name__ == "__main__":
+    import sys
+    no_of_args = len(sys.argv) - 1
+    if no_of_args == 1:
+        print("{} argument:".format(no_of_args))
+    elif no_of_args == 0:
+        print("{} argument.".format(no_of_args), end="")
+    elif no_of_args > 1:
+        print("{} arguments:".format(no_of_args))
 
-if no_of_args > 1:
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if no_of_args > 1:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
 
 
 
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-no_of_args = len(sys.argv)
-args_sum = 0
+if __name__ == "__main__":
+    import sys
+    no_of_args = len(sys.argv)
+    args_sum = 0
 
-for i in range(1, no_of_args):
-    args_sum += int(sys.argv[i])
-print("{}".format(args_sum))
+    for i in range(1, no_of_args):
+        args_sum += int(sys.argv[i])
+    print("{}".format(args_sum))
 
 
 
