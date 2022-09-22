4913d1f82ac6c78c7b608576d8e1d2f69186f933
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-22 21:15:44 +0000

    quick fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,9 @@ if __name__ == "__main__":
         print("{:d} argument:".format(no_of_args))
     elif no_of_args == 0:
         print("{:d} argument.".format(no_of_args), end="")
-    elif no_of_args > 1:
+    elif int(no_of_args) > 1:
         print("{:d} arguments:".format(no_of_args))
 
-    if no_of_args > 1:
+    if int(no_of_args) > 1:
         for i in range(1, len(sys.argv)):
-            print("{:d}: {:d}".format(i, sys.argv[i]))
-
-
-
+            print("{:d}: {:s}".format(i, sys.argv[i]))
