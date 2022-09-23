5fd7bcbbb49a6083fbf8f7e4ff2c5ae243bebfd6
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-23 18:34:43 +0000

    hot fix

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    len_args = len(sys.argv) - 1
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if len_args < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if operator == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            exit(0)
+        elif operator == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+            exit(0)
+        elif operator == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+            exit (0)
+        elif operator == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+            exit (0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
