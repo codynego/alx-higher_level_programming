b3e6ea99c5e1aa3a65203b38fa7d7a2a8662c645
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-25 18:21:07 +0000

    hot fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    idx_count = 0
-    if  idx < 0 and idx > len(my_list):
+    if  idx < 0 and idx >= len(my_list):
         return None
-    for i in my_list:
-        if idx_count == idx:
-            print("Element at index {:d} is {}".format(idx, i))
-        idx_count += 1
+    return my_list[idx]
 
