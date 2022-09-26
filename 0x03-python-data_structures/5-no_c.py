ed02d018cf66e18523077e43d3edc02e332860f0
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-26 23:03:23 +0000

    hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i is not 'c' or i is not 'C':
+        if i == 'c' or i == 'C':
+            continue
+        else:
             new_string += i
     return new_string
