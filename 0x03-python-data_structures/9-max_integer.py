227f1c95247b03afdc9a557f85e1349b2ff3e4ba
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-26 11:20:15 +0000

    added max_integer file

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
+    max_int = my_list[0]
     if not my_list:
         return None
     for i in my_list:
-        max_int = i if i > max_int else max_int
+        if i > max_int:
+            max_int = i
     return max_int
