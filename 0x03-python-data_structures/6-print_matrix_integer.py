c24ae9b24d8e7443b82575f18de0012b5288a2ef
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-26 23:57:13 +0000

    fix an error in print_matrix file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    j = 0
     for i in matrix:
-        for j in range(0, len(i) - 1):
+        for j in range(0, len(i) -1):
             print("{}".format(i[j]),end=" ")
-
+        print("{}".format(i[j + 1]))
