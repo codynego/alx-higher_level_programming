d22b54fa2957d57096cdca932e6a11fc3f5b4e63
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-27 03:01:10 +0000

    quick fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in range(0, len(i) -1):
-            print("{}".format(i[j]),end=" ")
-        print("{}".format(i[j + 1]))
