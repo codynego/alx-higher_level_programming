b36cfee4815ec756eded318cda3eb618d890cfca
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-27 01:31:05 +0000

    quick fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
             exit(0)
         elif operator == '*':
             print("{} * {} = {}".format(a, b, mul(a, b)))
-            exit (0)
+            exit(0)
         elif operator == '/':
             print("{} / {} = {}".format(a, b, div(a, b)))
-            exit (0)
+            exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
