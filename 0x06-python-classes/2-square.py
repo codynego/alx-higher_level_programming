c797f7eb11c745d6680900f6f2de2d1ad17dfe94
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-12 21:22:38 +0000

    added 2-square file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         size (int): The size of the square
     '''
     def __init__(self, size=0):
-        if type(size) is not int:
+        if type(size) is not "int":
             raise TypeError("size must be integer")
         if size < 0:
             raise ValueError("size must be >= 0")
