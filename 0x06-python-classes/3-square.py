51e31395399a9d3440e9a834370cefe6e1cbca45
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-12 21:33:55 +0000

    added 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+''' A class that defines a square '''
+
+
+class Square:
+    '''
+        Initialization of instance attributes
+
+        Args:
+        size (int): The size of the square
+    '''
+    def __init__(self, size=0):
+        '''
+            initialization
+        '''
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+            Calculates the area of a square
+
+            Return: The current square area
+        '''
+
+        return self.__size * self.__size
