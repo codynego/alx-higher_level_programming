699e4cad1623646bff907ddc151182f5be395ffa
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-12 19:08:34 +0000

    hot fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+'''A class that defines a square'''
 
 class Square():
-    """
-        A class that defines a Square
-    """
+    '''
+        An empty class
+    '''
 
     pass
