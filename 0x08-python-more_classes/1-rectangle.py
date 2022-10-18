98e08a813fac54e1155633caa19c91cad4fed358
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-18 10:15:58 +0000

    added 1-rectangle file

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,67 @@
+#/usr/bin/python3
+
+""" A class that defines a rectangle """
+
+
+class Rectangle:
+    """ Defining a class attribute """
+
+    def __init__(self, width=0, height=0):
+        """ 
+            Initializing an instance of the class
+        """
+        if not isinstance(width, int) or not isinstance(width, float):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int) or not isinstance(height, float):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """
+            Retriving the private attribute width
+            return: the width
+        """
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """
+            Setting the value of width
+            Args:
+            value (int): the new width
+
+            return: no return
+        """
+        if not isinstance(value, int) or not isinstance(value, float):
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("value must be >= 0")
+        self._width = value
+
+
+    @property
+    def height(self):
+        """
+            Retrieve the value of height
+        """
+        return height
+
+    @height.setter
+    def height(self, height):
+        """
+            Update the value of height
+            Args:
+            value (int): the new value of height
+        """
+        if not isinstance(value, int) or not isinstance(value, float):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = height
