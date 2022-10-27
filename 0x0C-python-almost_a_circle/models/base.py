81aff841af76034fb3378082d8fb9ad4410798db
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-27 11:12:07 +0000

    added a base class of the project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" A base class """
+
+
+class Base:
+
+    """ The base class """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            initializing the instance attributes
+            Args:
+            id (int): object id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
