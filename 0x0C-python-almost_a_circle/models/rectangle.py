5fbdeadede16dd3c3256c00084e2e851148a9e8f
Abednego Emonena <emonenaabednego@gmail.com>
2022-11-01 12:55:49 +0000

    hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,7 +34,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
-        self.__width = width
+        self.width = width
 
     @property
     def height(self):
@@ -55,7 +55,7 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be > 0")
-        self.__height = height
+        self.height = height
 
     @property
     def x(self):
